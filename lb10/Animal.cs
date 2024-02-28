214f39bf4ee79b07a54d6a4595bcc7c0e3130fbf
ярослав222 поликарпов <lo_ken@bk.ru>
2024-02-28 14:23:26 +0300

    Добавьте файлы проекта.

---
+++ lb10/Animal.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lb9
+{
+    internal class Animal
+    {
+        public string _Name;
+        public int _Age;
+        public double[] _WeightLast3Mounth = new double[3];
+        public string _Type;
+        public string _NumberPhone;
+
+        /// <summary>
+        /// Конструктор класса без параметров
+        /// </summary>
+        public Animal()
+        {
+            _Name = "Ник";
+            _Age = 4;
+            _WeightLast3Mounth = Generation_weight(_WeightLast3Mounth);
+            _Type = "Собака";
+            _NumberPhone = "+79630991494";
+        }
+
+        /// <summary>
+        /// Конструктор класса с одним параметром
+        /// </summary>
+        /// <param name="name">Кличка животного</param>
+        public Animal(string name)
+        {
+            _Name = name;
+            _Age = 4;
+            _WeightLast3Mounth = Generation_weight(_WeightLast3Mounth);
+            _Type = "Собака";
+            _NumberPhone = "+79630991494";
+        }
+
+        /// <summary>
+        /// Конструктор класса с двумя параметрами
+        /// </summary>
+        /// <param name="name">Кличка животного</param>
+        /// <param name="age">Возраст животного</param>
+        public Animal(string name, int age)
+        {
+            _Name = name;
+            _Age = age;
+            _WeightLast3Mounth = Generation_weight(_WeightLast3Mounth);
+            _Type = "Собака";
+            _NumberPhone = "+79630991494";
+        }
+
+        /// <summary>
+        /// Конструктор класса со всеми параметрами
+        /// </summary>
+        /// <param name="name">Кличка животного</param>
+        /// <param name="age">Возраст</param>
+        /// <param name="weightLast3Mounth">Вес за последние 3 месяца</param>
+        /// <param name="type">Вес животного</param>
+        /// <param name="numberPhone">Номер телефона владельца</param>
+        public Animal(string name, int age, double[] weightLast3Mounth, string type, string numberPhone)
+        {
+            _Name = name;
+            _Age = age;
+            _WeightLast3Mounth = weightLast3Mounth;
+            _Type = type;
+            _NumberPhone = numberPhone;
+        }
+
+        /// <summary>
+        /// Генератор веса животного за 3 месяца
+        /// </summary>
+        /// <param name="arr">Вес животного за 3 месяца(пустой массив)</param>
+        /// <returns>Вес животного за 3 месяца(заполненый массив)</returns>
+        public static double[] Generation_weight(double[] arr)
+        {
+            Random rnd = new Random(Guid.NewGuid().GetHashCode());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int a = rnd.Next(3, 30);
+                double b = rnd.Next(1, 99);
+                arr[i] = a + (b / 100);
+            }
+            return arr;
+        }
+    }
+}
---
+++ lb10/VetClinic.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace lb10
+{
+    partial class VetClinic
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelAge = new System.Windows.Forms.Label();
+            this.labelWeightLast3Mounth = new System.Windows.Forms.Label();
+            this.labelType = new System.Windows.Forms.Label();
+            this.labelNumberPhone = new System.Windows.Forms.Label();
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(55, 74);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(106, 16);
+            this.labelName.TabIndex = 0;
+            this.labelName.Text = "Имя животного";
+            // 
+            // labelAge
+            // 
+            this.labelAge.AutoSize = true;
+            this.labelAge.Location = new System.Drawing.Point(55, 136);
+            this.labelAge.Name = "labelAge";
+            this.labelAge.Size = new System.Drawing.Size(135, 16);
+            this.labelAge.TabIndex = 1;
+            this.labelAge.Text = "Возраст животного";
+            // 
+            // labelWeightLast3Mounth
+            // 
+            this.labelWeightLast3Mounth.AutoSize = true;
+            this.labelWeightLast3Mounth.Location = new System.Drawing.Point(55, 192);
+            this.labelWeightLast3Mounth.Name = "labelWeightLast3Mounth";
+            this.labelWeightLast3Mounth.Size = new System.Drawing.Size(138, 32);
+            this.labelWeightLast3Mounth.TabIndex = 2;
+            this.labelWeightLast3Mounth.Text = "Вес животного за \r\nпоследние 3 месяца";
+            // 
+            // labelType
+            // 
+            this.labelType.AutoSize = true;
+            this.labelType.Location = new System.Drawing.Point(55, 264);
+            this.labelType.Name = "labelType";
+            this.labelType.Size = new System.Drawing.Size(32, 16);
+            this.labelType.TabIndex = 3;
+            this.labelType.Text = "Вид";
+            // 
+            // labelNumberPhone
+            // 
+            this.labelNumberPhone.AutoSize = true;
+            this.labelNumberPhone.Location = new System.Drawing.Point(55, 308);
+            this.labelNumberPhone.Name = "labelNumberPhone";
+            this.labelNumberPhone.Size = new System.Drawing.Size(175, 16);
+            this.labelNumberPhone.TabIndex = 4;
+            this.labelNumberPhone.Text = "Номер телефона хозяина";
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(115, 364);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(75, 23);
+            this.buttonCreate.TabIndex = 5;
+            this.buttonCreate.Text = "Создать";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            // 
+            // VetClinic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1118, 477);
+            this.Controls.Add(this.buttonCreate);
+            this.Controls.Add(this.labelNumberPhone);
+            this.Controls.Add(this.labelType);
+            this.Controls.Add(this.labelWeightLast3Mounth);
+            this.Controls.Add(this.labelAge);
+            this.Controls.Add(this.labelName);
+            this.Name = "VetClinic";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelAge;
+        private System.Windows.Forms.Label labelWeightLast3Mounth;
+        private System.Windows.Forms.Label labelType;
+        private System.Windows.Forms.Label labelNumberPhone;
+        private System.Windows.Forms.Button buttonCreate;
+    }
+}
+
